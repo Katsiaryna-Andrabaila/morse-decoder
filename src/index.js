4ec243369ca26e3ee9ba8ccb33c82f48de9f1be6
katsiaryna-andrabaila <androbailoe@gmail.com>
2022-07-15 22:38:52 +0300

    feat: Morse-decoder done

--- src/index.js
+++ src/index.js
@@ -35,11 +35,26 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let symb = '';
+      for (let i = 0; i < expr.length; i += 10) {
+        symb = symb + ',' + expr.slice(i, i + 10).replace(/10/g, '.').replace(/11/g, '-').replace(/0/g, '');
+      }
+    let arr = symb.split(',').slice(1);
+   
+    let keys = Object.keys(MORSE_TABLE);
+    for (let i = 0; i < Object.keys(MORSE_TABLE).length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (keys[i] === arr[j]) {
+          arr.splice(j, 1, MORSE_TABLE[keys[i]]);
+        }
+      }
+    }
+    return arr.join('');
+};
 
 module.exports = {
     decode
